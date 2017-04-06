08b65696af99846706331e2bd7b414698cb8ef94
isennett <isabelle.sennett@tufts.edu>
2017-04-05 23:57:35 -0400

    all correct shapes and three puzzles

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ window.onload = function() {
     });
 
     var big_orange_triangle = new Konva.Line({
-      points: [100, 100, 300, 100, 200, 0, 0, 0],
+      points: [0, 0, 400, 0, 200, 200],
       x: startX*.69,
       y: startY*.66,
       fill: '#FF9900',
@@ -45,7 +45,7 @@ window.onload = function() {
     });
 
     var big_green_triangle = new Konva.Line({
-      points: [100, 100, 300, 100, 200, 0, 0, 0],
+      points: [0, 0, 200, 200, 0, 400],
       x: startX*1.46,
       y: startY*1.3,
       fill: '#269B7E',
@@ -68,7 +68,7 @@ window.onload = function() {
     });
 
     var small_purple_triangle = new Konva.Line({
-      points: [100, 100, 300, 100, 200, 0, 0, 0],
+      points: [100, 0, 200, 100, 0, 100],
       x: startX/4,
       y: startY/4,
       fill: '#7E36EF',
@@ -80,7 +80,7 @@ window.onload = function() {
 
 
     var small_pink_triangle = new Konva.Line({
-      points: [100, 100, 300, 100, 200, 0, 0, 0],
+      points: [100, 0, 100, 200, 0, 100],
       x: startX/4,
       y: startY,
       fill: '#EA5CCF',
@@ -92,7 +92,7 @@ window.onload = function() {
 
 
     var medium_red_triangle = new Konva.Line({
-      points: [100, 100, 300, 100, 200, 0, 0, 0],
+      points: [200, 0, 200, 200, 0, 200],
       x: startX/.8,
       y: startY*.8,
       fill: '#F83249',
@@ -102,39 +102,39 @@ window.onload = function() {
       draggable: true,
     });
 
-document.getElementById('square').onclick = function() { 
-    console.log('square');
-    rhombus.points();
-    big_orange_triangle.points();
-    big_green_triangle.points();
-    square.points();
-    small_purple_triangle.points();
-    small_pink_triangle.points();
-    medium_red_triangle.points();
+document.getElementById('swan').onclick = function() { 
+    console.log('swan');
+    rhombus.points([100, 0, 100, 200, 0, 300, 0, 100]);
+    big_orange_triangle.points([200, 0, 400, 200, 0, 200]);
+    big_green_triangle.points([0, 0, 400, 0, 200, 200]);
+    square.points([100, 0, 200, 100, 100, 200, 0, 100]);
+    small_purple_triangle.points([100, 0, 200, 100, 0, 100]);
+    small_pink_triangle.points([100, 0, 200, 100, 0, 100]);
+    medium_red_triangle.points([0, 0, 300, 0, 150, 150]);
     layer.draw();
 };
 
-document.getElementById('swan').onclick = function() { 
-    console.log('swan');
-    rhombus.points();
-    big_orange_triangle.points();
-    big_green_triangle.points();
-    square.points();
-    small_purple_triangle.points();
-    small_pink_triangle.points();
-    medium_red_triangle.points();
+document.getElementById('square').onclick = function() { 
+    console.log('square');
+    rhombus.points([0, 100, 100, 0, 300, 0, 200, 100]);
+    big_orange_triangle.points([0, 0, 400, 0, 200, 200]);
+    big_green_triangle.points([0, 0, 200, 200, 0, 400]);
+    square.points([100, 0, 200, 100, 100, 200, 0, 100]);
+    small_purple_triangle.points([100, 0, 200, 100, 0, 100]);
+    small_pink_triangle.points([100, 0, 100, 200, 0, 100]);
+    medium_red_triangle.points([200, 0, 200, 200, 0, 200]);
     layer.draw();
 };
 
 document.getElementById('fish').onclick = function() { 
     console.log('fish');
-    rhombus.points();
-    big_orange_triangle.points();
-    big_green_triangle.points();
-    square.points();
-    small_purple_triangle.points();
-    small_pink_triangle.points();
-    medium_red_triangle.points();
+    rhombus.points([100, 0, 100, 200, 0, 300, 0, 100]);
+    big_orange_triangle.points([300, 0, 300, 300, 0, 300]);
+    big_green_triangle.points([0, 0, 300, 0, 300, 300]);
+    square.points([0, 0, 150, 0, 150, 150, 0, 150]);
+    small_purple_triangle.points([0, 0, 150, 0, 0, 150]);
+    small_pink_triangle.points([0, 0, 150, 150, 0, 150]);
+    medium_red_triangle.points([150, 0, 150, 300, 0, 150]);
     layer.draw();
 };
 
